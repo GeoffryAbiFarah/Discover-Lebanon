8143eae2b9b097f4f006f4b6c3f5b2f8ac530f65
Anthony Chedid <anthonyaachedid@gmail.com>
2021-11-22 20:07:01 +0200

    unit tests

--- Backend/src/test/java/com/example/demo/KhedniMa3akApplicationTests.java
+++ Backend/src/test/java/com/example/demo/KhedniMa3akApplicationTests.java
@@ -1,13 +1,106 @@
 package com.example.demo;
 
+import java.io.IOException;
+
+
+import com.example.demo.models.Place;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class KhedniMa3akApplicationTests {
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	protected MockMvc mvc;
+
+
+	public void setup() {
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
 	@Test
-	void contextLoads() {
+	public void getAllPlaces() throws Exception {
+		String uri="/places";
+
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+	}
+
+	@Test
+	public void getAllPlacesByNameTypeArea() throws Exception {
+		String uri="/places-by-name-and-type-and-area/test/test/test";
+
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+	}
+	@Test
+	public void getAllPlacesByName() throws Exception {
+		String uri="/places-by-name/test";
+
+
+
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+	}
+	@Test
+	public void getAllPlacesByType() throws Exception {
+		String uri="/places-by-type/test";
+
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+	}
+	@Test
+	public void getAllPlacesByArea() throws Exception {
+		String uri="/places-by-area/test";
+
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
 	}
 
 }
